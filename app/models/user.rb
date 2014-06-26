3dc753eab883fc2c67846caf32720f3dc3d121e2
Sohin Chhatrala <sohinc123@yahoo.com>
2014-06-25 21:10:30 -0500

    updated frontend for the new authentication system
    added simple_form
    update several forms to bootstrap/simple_form
    added login/logout/register/profile links to our nav
    added a collection select to our status form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :profile_name
+  # attr_accessible :title, :body
+
+  has_many :statuses
+
+  def full_name
+  	first_name + " " + last_name
+  end
 end
